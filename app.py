9ae079ee838278f37ea199570c915be21f85f161
Nathan Cherny <84670821+Nathan-Cherny@users.noreply.github.com>
2026-03-03 16:29:06 -0500

    Implement glucose reading endpoint
    Added a route to get glucose readings from Dexcom API.

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+from pydexcom import Dexcom
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "test"
+def get_glucose():
+    # try:
+    dexcom = Dexcom(username=os.environ.get("USERNAME"), password=os.environ.get("PASSWORD"))
+    reading = dexcom.get_current_glucose_reading()
+    return reading.json
+    # except Exception as e:
+    #     print(e)
+    #     return {"Error": f"Something went wrong: {e}"}
+
+@app.route("/get_glucose")
+def glucose():
+    return jsonify(get_glucose())
+
+@app.route("/")
+def index():
+    return "<h1>Dexcom api python call</h1><p>Do not use without permission. xSoTec</p>"
