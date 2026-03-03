fd82379c3e217621aaeb91a675b4f512090504de
Nathan Cherny <84670821+Nathan-Cherny@users.noreply.github.com>
2026-03-03 16:42:17 -0500

    Refactor get_glucose function for error handling

--- app.py
+++ app.py
@@ -5,16 +5,13 @@ from pydexcom import Dexcom
 app = Flask(__name__)
 
 def get_glucose():
-    # try:
-    print(os.environ)
-    print(os.environ.get("USERNAME"))
-    
-    dexcom = Dexcom(username=os.environ.get("USERNAME"), password=os.environ.get("PASSWORD"))
-    reading = dexcom.get_current_glucose_reading()
-    return reading.json
-    # except Exception as e:
-    #     print(e)
-    #     return {"Error": f"Something went wrong: {e}"}
+    try:
+        dexcom = Dexcom(username=os.environ.get("USERNAME"), password=os.environ.get("PASSWORD"))
+        reading = dexcom.get_current_glucose_reading()
+        return reading.json
+    except Exception as e:
+        print(e)
+        return {"Error": f"Something went wrong: {e}"}
 
 @app.route("/get_glucose")
 def glucose():
